f81be2d46d0ab6105c407d999e45c9dabf0d886b
MarcusYA <marcus.78@hotmail.com>
2023-03-24 16:05:32 +0000

    f

---
+++ index.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById(computer-choice)
+const yourChoice = document.getElementById(your-choice)
+const result = document.getElementById(result)
+const possibleChoice.forEach
