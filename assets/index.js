6e474a811cfa77dc1d7d06647118dfd11e3d193d
MarcusYA <marcus.78@hotmail.com>
2023-03-25 16:30:12 +0000

    adf

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+const computerChoice = document.getElementById(computer-choice)
+const yourChoice = document.getElementById(your-choice)
+const result = document.getElementById(result)
+
+const alert1 = document.getElementById('alert');
+
+function infbox() {
+    alert('I love pdf!');
+}
+
+alert1.addEventListener("click", infbox);
+
+document.getElementById("alert1").addEventListener("click", function(){
+    alert("klickande")
+});
--- index.js
+++
@@ -1,4 +0,0 @@
-const computerChoice = document.getElementById(computer-choice)
-const yourChoice = document.getElementById(your-choice)
-const result = document.getElementById(result)
-const possibleChoice.forEach
